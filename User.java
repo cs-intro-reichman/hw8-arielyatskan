ab23a1762bc67f1d265f982c6a3662a9702843c8
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2026-01-01 22:24:01 +0200

    Done

--- User.java
+++ User.java
@@ -51,7 +51,7 @@
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        if(fCount < 10 && !follows(name)) {
+        if(fCount < maxfCount && !follows(name)) {
             follows[fCount] = name;
             fCount++;
             return true;
